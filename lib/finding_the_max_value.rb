a550c3a276a29bc60d89ead36ec42eed0c92d47a
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-04 22:06:18 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,10 @@
 def find_max_value(array)
-  
+  length = array.length
+  starting value = array[0]
+  length.times do |index|
+    if array[index] > value_to_find
+      value_to_find == array[index]
+      return value_to_find
+    end
+  end
 end
